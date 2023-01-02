3c6f5fd00bc3fca1fb576a5eb3aae8644e133068
leandroArraes <leandro.arraes.182@gmail.com>
2023-01-02 12:07:50 -0300

    Commit inicial

---
+++ dist/js/index.js
@@ -0,0 +1,13 @@
+"use strict";
+// caracteristica do typescript é definir tipo de variavel 
+// inferencia x annotation
+// inferencia
+// é  Quando uma variavel é decladar sem a tipagem
+// ex:
+let letra = 'variavel inferida como tipada como string';
+// annotation
+// é quando se declara o tipo da variavel
+// ex:
+let numero = 10;
+let decladroLetras = 'variavel declarada como sendo do tipo String';
+console.log(letra);
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// caracteristica do typescript é definir tipo de variavel 
+
+
+// inferencia x annotation
+    // inferencia
+        // é  Quando uma variavel é decladar sem a tipagem
+        // ex:
+        let letra = 'variavel inferida como tipada como string'
+
+// annotation
+        // é quando se declara o tipo da variavel
+        // ex:
+        let numero: Number = 10
+        let decladroLetras: String = 'variavel declarada como sendo do tipo String'
+
+
+console.log(letra);
